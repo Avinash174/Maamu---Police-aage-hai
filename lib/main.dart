8ab56513ffd943848595eb00fe0a8dc8ce7f0cd3
Avinash174 <avinashmagar15@gmail.com>
2023-12-12 21:53:10 +0530

    new changes done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mamu_police_aage_hai/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: SplashScreen(),
     );
   }
 }
